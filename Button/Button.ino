a926c9671007e02022f68ab0dfb72abe0c2da149
Pantheon-corp <81566953+Pantheon-corp@users.noreply.github.com>
2021-04-24 15:51:34 +0300

    Button 1.2

--- Button/Button.ino
+++ Button/Button.ino
@@ -1,24 +1,40 @@
+/* 
+ * Project name         Button
+ * Author:              Lanity Roshoose
+ * Date of mod.         12.04.2021
+ * Version              beta 1.2
+ */
+
+const int AMOUNT = 8;
+int LEDs[AMOUNT] = {4, 5, 6, 7, 8, 9, 10, 11};
+int btn = 2;
+const int DELAY = 500;
+
+
 void setup(){
-  for(int i = 4, i <= 11; i++){
-    pinMode(i, OUTPUT);
+  for(int i = 0; i < AMOUNT; i++){
+    pinMode(LEDs[i], OUTPUT);
   }
-  pinMode(6, INPUT);
+  pinMode(btn, INPUT_PULLUP);
 }
 
 void loop(){
-  for(int i = 4, i <= 11; i++){
-    if(digitalRead(6) != HIGH){
+  for(int i = 0; i < AMOUNT; i++){
+    if(digitalRead(btn) == HIGH){
       break;
     }
-    digitalWrite(i, HIGH);
-    delay(500);
-    if (i == 11){
-      while(true){
-        for(int j = 4; j <= 11; j++){
-          digitalWrite(j, !digitalRead(j));
+    digitalWrite(LEDs[i], HIGH);
+    delay(DELAY);
+    if(i+1 >= AMOUNT){
+      while("Pantheon is the best"){
+        for(int j = 0; j < AMOUNT; j++){
+          digitalWrite(LEDs[j], !digitalRead(LEDs[j]));
         }
-        delay(250);
+        delay(DELAY);
       }
     }
+  }
+  for(int i = 0; i < AMOUNT; i++){
+    digitalWrite(LEDs[i], LOW);
   }
 }
