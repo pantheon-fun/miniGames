c549378bfef1b472ec3b46c2a2624c3450e33fec
Lanity <damned89098@gmail.com>
2021-06-13 13:50:42 +0300

    v 0.3

--- WireConnect/main.ino
+++ WireConnect/main.ino
@@ -1,128 +1,87 @@
-#include<PantheonDuino.h>
-int leds1 = 0;
-int leds2 = 0;
-char arrColors[4] = {'r', 'g', 'b', 'p'};
-float Vin = 5.0;
-int R1 = 10000;
-byte shotArr[2];
-int sum;
+//3 - LED
+//4,5,6,7,8 - 1 gr.
+//9,10,11,12,13 - 2 gr.
 
 
-int rgbStart(char color, int number) // Возвращает индекс напряжения
-{  
-    int innerLeds = 0; // Внутренняя переменная для работы со сдвиговым массивом
-    int retVal = 0; // Переменная для возврата значения из функции
-    if(color == 'r'){
-        innerLeds += 1;
-        retVal = 0;
-    } else if (color == 'g') {
-        innerLeds += 2;
-        retVal = 1;
-    } else if (color == 'b') {
-        innerLeds += 4;
-        retVal = 2;
-    } else if (color == 'p') {
-        innerLeds += 5;
-    }
-    if(number == 1){
-        leds1 += innerLeds;
-    } else if (number == 2){
-        leds1 += innerLeds * 8;
-    } else if (number == 3){
-        leds2 += innerLeds;
-    } else if (number == 4){
-        leds2 += innerLeds * 8;
-    }
+
+//LED defines
+#define LED_PIN 3
+#define LED_NUM 10
+#include "Adafruit_NeoPixel.h"
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_NUM, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define RED strip.Color(255,0,0)
+#define GREEN strip.Color(0,255,0)
+#define BLUE strip.Color(0,0,255)
+#define FUKSIA strip.Color(255,0,255)
+#define WHITE strip.Color(255,255,255)
+
+
+
+int groupO[5] = {4,5,6,7,8};
+int groupI[5] = {9,10,11,12,13};
+uint32_t colorsO[5] = {RED, GREEN, BLUE, FUKSIA, WHITE};
+uint32_t colorsI[5] = {RED, GREEN, BLUE, FUKSIA, WHITE};
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  for(int i = 0; i < 5; i++){
+    pinMode(groupO, OUTPUT);
+    pinMode(groupI, INPUT);
+  }
+  randomSeed(analogRead(0));
+  for(int i = 0; i < 5; i++){
+    int j = random(0,5);
+    Serial.println(j);
+    uint32_t tmp = colorsO[j];
+    colorsO[j] = colorsO[i];
+    colorsO[i] = tmp;
+    j = random(0,5);
+    Serial.println(j);
+    tmp = colorsI[j];
+    colorsI[j] = colorsI[i];
+    colorsI[i] = tmp;
+  }
+  strip.begin();
+  strip.setBrightness(255);
+  strip.clear();
+  for(int i = 0; i < 5; i++){
+    strip.setPixelColor(i, colorsO[i]);
+    strip.setPixelColor(i+5, colorsI[i]);
+  }
+  strip.show();
+  //WORKING
 }
 
-void swap( char &left, char &right )  // Вспомогательная функция для перемешивания массива
-{
-char tmp = left;
-left = right;
-right = tmp;
-}
-
-void setup() {
-    Serial.begin(9600);
-    //getShot(7, &Serial, shotArr);
-    randomSeed(analogRead(5)); // Случайный сид рандома
-    for(size_t i = 0; i < 4; i++) { 
-        swap(arrColors[i], arrColors[random(0, 3)]);
-    }
-    for(int i = 0; i < 4; i++){
-        rgbStart(arrColors[i], i+1);
-        Serial.println(arrColors[i]);
-    }
-    digitalWrite(6, LOW);
-    shiftOut(9, 10, MSBFIRST, leds1);
-    shiftOut(7, 8, MSBFIRST, leds2);
-//    shiftOut(11, 12, MSBFIRST, leds1);
-//    shiftOut(9, 10, MSBFIRST, leds2);
-    digitalWrite(6, HIGH);
-}
-
-
-int checkResist(int pin, char color){
-    int raw = analogRead(pin);
-    if (raw) {
-        float Vout = (raw*Vin)/1024.0;
-        float R2 = R1*((Vin/Vout)-1);
-        Serial.println((String)pin + " " + (String)R2);
-        //Serial.println("V2 = " + (String)Vin);
-        //Serial.println("raw data = " + (String)raw);
-        delay(50);
-        if(color == 'r'){
-            if(R2 < 400) {return 1;}
-        } else if (color == 'g'){
-            if(R2 > 600 && R2 < 1800){return 1;}
-        } else if (color == 'b'){
-            if(R2 > 8000 && R2 < 12000){return 1;}
-        } else if (color == 'p'){
-            if(R2 > 60000){return 1;}
-        } 
-    } else {
-            return 0;
+void loop(){
+  for(int i = 0; i < 5; i++){
+    digitalWrite(groupO[i], LOW);
+  }
+  int done = 0;
+  int sum = 0;
+  for(int i = 0; i < 5; i++){
+    digitalWrite(groupO[i], HIGH);
+    for(int j = 0; j < 5; j++){
+      if(digitalRead(groupI[j]) == HIGH){
+        sum++;
+        if(colorsO[i] == colorsI[j]){
+          done++;
         }
-}
-
-void blink(int pin){
-    for(int i = 0; i < 15; i++){
-        digitalWrite(pin, HIGH);
-        delay(50);
-        digitalWrite(pin, LOW);
-        delay(50);
+      }
     }
-}
-
-void(* resetFunc) (void) = 0; // Функция перезапуска
-
-void loop() {
-    // A0 - 1, A1 - 2, A3 - 3, A4 - 4
-    // r - 0 ohm
-    // g - 1k ohm
-    // b - 10k ohm
-    // p - 100k ohm
-    sum = 0;
-    for(int i = 0; i < 4; i++){
-        int add = checkResist(i, arrColors[i]);
-        sum += add;
-        Serial.println("Add = " + (String)add + "; sum = " + (String)sum);
-        add = 0;
+  }
+  done = 5;
+  sum = 5;
+  if(done == sum && sum == 5){
+    while(true){
+      int i = random(0,10);
+      uint32_t old = strip.getPixelColor(i);
+      strip.setPixelColor(i, 0, 0, 0);
+      strip.show();
+      delay(150);
+      strip.setPixelColor(i, old);
+      strip.show();
     }
-    Serial.println("Sum is " + (String)sum);
-    if(sum == 4){
-        byte arr[2];
-        digitalWrite(2, HIGH);
-        Serial.println("WINNER WINNER CHICKEN DINNER");
-        delay(10000000000);
-        //getShot(7, &Serial, shotArr);
-        //if(arr[0] == shotArr[0] && arr[1] == shotArr[1]){
-            //digitalWrite(2, HIGH); // Светодиод что всё выполнено
-            //delay(10000000000); // Остановка "навсегда"
-        //} else {
-            //blink(2);
-            //resetFunc();
-        //}
-    }
-    delay(1000);
+  }
 }
